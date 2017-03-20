c282027be23e2e95cb879c775510dd54ce6daad6
JustinClough <clougj@rpi.edu>
2017-03-20 19:49:03 -0400

    Does A1 operations 3 (sort of) and 4

--- a1/a1mix.cc
+++ a1/a1mix.cc
@@ -1,5 +1,11 @@
+//PUMI headers
 #include <PCU.h>
 #include <pumi.h>
+
+// STL headers
+#include <vector>
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
@@ -7,14 +13,38 @@ int main(int argc, char** argv)
   pumi_start();
   pGeom geom = pumi_geom_load("cube.dmg", "mesh");
   pMesh mesh = pumi_mesh_load(geom, "mixed-mesh-1.smb",1);
-  //
 
+  // Create a list to Write to
+  std::ofstream Op4Results;
+  Op4Results.open("a1_operation4_results.txt");
+  Op4Results << "GlobalID's of Mesh Faces classified on Model Face 81:\n";
+
+  // Operation 4: Print list of mesh faces on model face 81
   int modFaceNum = 81; // Model face number according to handout
+  pGeomEnt gFace81;
 
+  // Loop over all model faces to find the one global ID of interest
+  //    (couldn't find an API to go from globalID to entity)
+  for(pGeomIter it=geom->begin(2); it!=geom->end(2); ++it)
+  {
+    // Get entity with matching globalID
+    if(pumi_gent_getID(*it) == modFaceNum)
+    {
+      gFace81 = *it;
+    }
+  }
 
+  // Get Mesh faces classified on model face
+  std::vector<pMeshEnt> meshFaces;
+  pumi_gent_getRevClas( gFace81, meshFaces);
+  
+  // Loop through the vector of mesh faces; print global_ID
+  for(int i=0; i<(int)meshFaces.size(); i++)
+  {
+    Op4Results << pumi_ment_getGlobalID( meshFaces[i]);
+    Op4Results << "\n";
+  }
 
-  // insert mesh query code here
-  //
   pumi_mesh_write(mesh,"outMixed","vtk");
   pumi_mesh_delete(mesh);
   pumi_finalize();
--- a1/a1quad.cc
+++ a1/a1quad.cc
@@ -35,10 +35,10 @@ int main(int argc, char** argv)
   double EdgeLength = 8.0;
   double Height = 4.0;
   double Width = 6.0;
-  double x = 0;
-  double y = 0; 
   double dw = EdgeLength/(Width);
   double dh = EdgeLength/(Height);
+  int Vert_Width = 1+(int)Width;
+  int Vert_Height = 1+(int)Height;
   pMeshEnt Vert;
   std::vector<pMeshEnt> Verts;
 
@@ -50,10 +50,10 @@ int main(int argc, char** argv)
 
   // Create all verteces
   // Loop over points along Height
-  for(int i = 0; i<Height+1; i++)
+  for(int i = 0; i<Vert_Height; i++)
   { 
     // Loop over points along width
-    for(int j = 0; j<Width+1; j++)
+    for(int j = 0; j<Vert_Width; j++)
     {
       // Define Coordinates for this point
       Coords[0] = j*dw;
@@ -73,7 +73,8 @@ int main(int argc, char** argv)
       Verts.push_back(Vert);
     }
   } 
-
+  
+  std::cout << "Finished Verteces Edges" << std::endl;
   std::cout << "Total Verteces placed: " << (int)Verts.size() << std::endl;
   
   pMeshEnt Vert_pair[2];
@@ -84,7 +85,7 @@ int main(int argc, char** argv)
   // Loop over ordered vector of verteces to create horizontal edges
   for(int i=0; i< (int)Verts.size(); i++)
   {
-    if( (i+1)%((int)(Width+1)) != 0 || i == 0)
+    if( (i+1)%(Vert_Width) != 0 || i == 0)
     {
       Vert_pair[0] = Verts[i];
       Vert_pair[1] = Verts[i+1];
@@ -93,15 +94,17 @@ int main(int argc, char** argv)
       Edges.push_back(edge);
     } // End if
   } // End for
+  
+  int Hor_Edges = Edges.size();
+  std::cout << "Finished Creating " << Hor_Edges << " Horizontal Edges" << std::endl;
 
   // Loop over ordered vector of verteces to create vertical edges
   for(int i=0; i< (int)Verts.size(); i++)
   {
-    if( i < (Width+1)*(Height))
+    if( i < (Vert_Width)*(Height))
     {
       Vert_pair[0] = Verts[i];
-      Vert_pair[1] = Verts[i+7];
-      Print_Coords_of_Vert( Vert_pair[0]);
+      Vert_pair[1] = Verts[i+Vert_Width];
       // Create edge
       edge = pumi_mesh_createEnt( mesh, NULL, 1, Vert_pair);
       Edges.push_back(edge);
@@ -109,30 +112,79 @@ int main(int argc, char** argv)
   } // End for
 
   std::cout << "Finished Creating Vertical Edges" << std::endl;
+  std::cout << "Total Edges placed: " << (int)Edges.size() << std::endl;
   
   // Create Edge Group to write to 
   pMeshEnt downward[4];
   
+  // Create Vector of Elements
+  std::vector<pMeshEnt> Elements;
+  pMeshEnt element;
+  
   // Create Topology
-  for( int i = 0; i< Width*Height; i++)
+  for( int i = 0; i< (Hor_Edges-(int)Width); i++)
   {
-
-
-
-
-
-
+    // Create group of downward adjecent edges
+    downward[0] = Edges[i];
+    downward[1] = Edges[i+Hor_Edges+1];
+    downward[2] = Edges[i+Vert_Width];
+    downward[3] = Edges[i+Hor_Edges];
+    // Create Element
+    element = pumi_mesh_createEnt( mesh, NULL, 3, downward);
+    Elements.push_back(element);
 
   }
+  int Tot_Elements = (int)Elements.size();
+  std::cout << "Finished Creating Element" << std::endl;
+  std::cout << "Total Elements placed: " << Tot_Elements << std::endl;
 
+  std::cout << "Assigning GlobalID\n" << std::endl;
+  // Assign Global Numbering scheme to finished mesh
+  pumi_mesh_createGlobalID( mesh );
+  int ID;
+
+  // Prep printout space for Element and Vertex numbers
+  Op3Results << "\nList of Elements and Vertex Members\n" ; 
+  Op3Results << "Element Number:\tVerteces on Element:\n" ; 
+  
+  // Create list of member verteces
+  std::vector<pMeshEnt> adj_Verts;
+  apf::MeshIterator* ElemIt = mesh->begin(2);
+  
+  while(( element = mesh->iterate(ElemIt)))
+  {
+    std::cout << "Getting Element GlobalID\n" << std::endl;
+    ID = (pumi_ment_getGlobalID( element ));
+    Op3Results << ID ;
+    Op3Results << "\t" ;
+    // Get dimension 0 (vertex) adjecntcies of element
+    pumi_ment_getAdj(element, 0, adj_Verts);
+    
+    for(int j=0; j< (pumi_ment_getNumAdj(element, 0)); j++)
+    {
+      Vert = adj_Verts[j];
+      std::cout << "Getting Vertex GlobalID \n" << std::endl;
+      Op3Results << (pumi_ment_getGlobalID( Vert )) ; 
+      Op3Results << "\t" ;
+      
+    }
+    Op3Results << "\n";
+  }
+  mesh->end(ElemIt);
 
   // Clean up
   Op3Results.close();
   pumi_mesh_freeze(mesh);
-  
+ 
+  std::cout << "Writing Mesh..." << std::endl; 
   pumi_mesh_write(mesh,"outQuad","vtk");
+  std::cout << "Finished Writing Mesh." << std::endl;
   pumi_mesh_delete(mesh);
   pumi_finalize();
   MPI_Finalize();
 }
 
+/* Questions for work session:
+ * 1. Crashes when writing <mesh>.vtk files; why?
+ * 2. Crashes when (attempting to) read GlobalID for elements; why?
+ */
