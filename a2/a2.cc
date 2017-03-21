766af35c61ba4cda7e2b785718e3aca18a025bac
JustinClough <clougj@rpi.edu>
2017-03-21 17:36:06 -0400

    Adds assigment 2 content; begins A2

---
+++ a2/a2.cc
@@ -0,0 +1,33 @@
+#include <PCU.h>
+#include <pumi.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool hasNode(pMesh m, pMeshEnt e)
+{
+  return pumi_shape_hasNode(pumi_mesh_getShape(m),pumi_ment_getTopo(e));
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 3) {
+    printf("usage: %s reorder_?.dmg reorder_?.smb\n", argv[0]);
+    return 0;
+  }
+
+  MPI_Init(&argc,&argv);
+  pumi_start();
+  pGeom geom = pumi_geom_load(argv[1],"mesh");
+  pMesh mesh = pumi_mesh_load(geom,argv[2],1);
+  if(!strcmp (argv[1], "reorder_c.dmg"))
+    pumi_mesh_setShape(mesh,pumi_shape_getLagrange(2));
+
+  //
+  // insert reordering (numbering) code here
+  //
+  pumi_mesh_write(mesh,"numbered","vtk");
+  pumi_mesh_delete(mesh);
+  pumi_finalize();
+  MPI_Finalize();
+}
+
