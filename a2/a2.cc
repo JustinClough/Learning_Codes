1d17c876e7d6bbde7e6e96ece5bbd81a8438be24
JustinClough <clougj@rpi.edu>
2017-03-23 08:15:59 -0400

    A2; finds starting vertex

--- a2/a2.cc
+++ a2/a2.cc
@@ -5,11 +5,27 @@
 // STL Headers
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <fstream>
 
 bool hasNode(pMesh m, pMeshEnt e)
 {
   return pumi_shape_hasNode(pumi_mesh_getShape(m),pumi_ment_getTopo(e));
 }
+
+double get_sq_magnitude( double* Coords)
+{
+  double sq_mag = 0.0;
+  // find sum of squares of components 
+  for (int i=0; i<3; i++)
+  {
+    sq_mag = sq_mag + (Coords[i])*(Coords[i]); 
+  }
+  return sq_mag;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -24,20 +40,71 @@ int main(int argc, char** argv)
   pMesh mesh = pumi_mesh_load(geom,argv[2],1);
   if(!strcmp (argv[1], "reorder_c.dmg"))
     pumi_mesh_setShape(mesh,pumi_shape_getLagrange(2));
-/*
-//// STEP 1:
-  // Find most extreme vertex
+
+//// STEP 1:  Find starting vertex
+  // Declare working variables
+  std::vector<pMeshEnt> model_Verts;
   std::vector<pMeshEnt> Verts;
-  int length_old = 0;
-  int length_now = 1;
-  while( length_old<length_now)
+  pMeshEnt Start_Vert;
+  std::vector<double*> Vert_Coords;
+  double Coords[3] = {0,0,0};
+  double Sum_Coords[3] = {0,0,0}; 
+  double Ave_Coords[3] = {0,0,0};
+  double* Dif_Coords[3] = {0,0,0};
+  double distance = 0.0;
+  double g_distance = 0.0;
+  double n = 0;
+
+  // For every model vertex...
+  for(pGeomIter it=geom->begin(1); it!=geom->end(1); ++it)
   {
+    // Get mesh vertex classified on model vertex
+    pumi_gent_getRevClas( *it, model_Verts);
+    Verts.push_back(model_Verts[0]);
 
-
+    // Update vertex set stats
+    pumi_node_getCoord( model_Verts[0], 0, Coords);
+    Vert_Coords.push_back(Coords); 
+    for(int i=0; i<3; i++)
+    {
+      Sum_Coords[i] = Coords[i];
+    }
+    n = n + 1.0;
   }
-  
-*/
 
+  // Get average location of all model vertices
+  for(int i=0; i<3; i++)
+  {
+    Ave_Coords[i] = Sum_Coords[i]/n;
+  }
+
+  // For every mesh vertex on model 
+  for( int i=0; i< (int)Verts.size(); i++)
+  {
+    // Get this Vertex's Coordinates
+    Coords = Vert_Coords[i];
+
+    // Compare this vertex's coordinates to the average; get the distance
+    for(int j=0; j<3; j++)
+    {
+      Dif_Coords[j] = Ave_Coords[j] - Coords[j];
+    } 
+    distance = get_sq_magnitude( Dif_Coords );
+    
+
+    // If this vertex is further from the center of all vertices than all others
+    //  (ties go to the home team, not runner)
+    if( distance > g_distance)
+    {
+      // Then, it is likely to be the most extreme vertex
+      Start_Vert = Verts[i];
+      // Update distance to beat
+      g_distance = distance;
+    } // ENDIF 
+  } // ENDFOR every vertex on the model vertices
+
+  // Clean up
+  Verts.clear();
 
 
   // Finish
