a4a3b35920d198c6223b6f909167b8c942fe2d7e
JustinClough <clougj@rpi.edu>
2017-05-15 20:24:10 -0400

    Creates and uses a class

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+class square{
+  private:
+    int length;
+  public:
+    square();
+    square(int);
+    int height;
+    int area (void)
+    {
+      return length*length;
+    }
+};
+
+square::square ()
+  {
+  length = 10;
+  }
+
+square::square( int a)
+  {
+    length = a;
+  }
+
 int main( int argc, char** argv ) {
+
+  using std::cout;
+  using std::endl;
+
   int b = 5;
   int* a = &b;
 
@@ -8,6 +35,14 @@ int main( int argc, char** argv ) {
   {
     std::cout << "i = " << i << std::endl;
   }
+
+  square sq_def;
+
+  cout << "Default Area = " << sq_def.area() << endl;
+
+  square sq1 (5);
+
+  cout << "Declared Area = " << sq1.area() << endl;
 
   return 0;
 }
