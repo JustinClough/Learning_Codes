c096597166a4d31f25966aba155a15dec6b390f0
JustinClough <clougj@rpi.edu>
2017-05-21 08:03:13 -0400

    Creates header for bjarne text, update .gitignore

---
+++ bjarne_text/chapter_3/bjarne.hpp
@@ -0,0 +1,14 @@
+#ifndef BJARNE_HPP
+#define BJARNE_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+#endif
