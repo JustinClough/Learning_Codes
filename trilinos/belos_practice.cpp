61aba72066359c8063d4368fd661cbb652f5fd6c
JustinClough <clougj@rpi.edu>
2017-05-24 09:55:09 -0400

    Fixes parameter list; actually solves problem now

--- trilinos/belos_practice.cpp
+++ trilinos/belos_practice.cpp
@@ -7,6 +7,7 @@
 #include "Tpetra_Map.hpp"
 #include <Tpetra_CrsMatrix.hpp>
 #include <Tpetra_DefaultPlatform.hpp>
+#include <MatrixMarket_Tpetra.hpp>
 
 #include <Teuchos_GlobalMPISession.hpp>
 #include <Teuchos_oblackholestream.hpp>
@@ -53,7 +54,10 @@ typedef Belos::LinearProblem
   <scalar_t, multiVec_t, Operator_t>                      LinearProblem;
 typedef Tpetra::RowMatrix
   <scalar_t, local_ordinal_t, global_ordinal_t, KNode>    row_mat_t;
+typedef Ifpack2::Factory                                  factory_t;
+typedef Tpetra::MatrixMarket::Writer<crs_mat_t>           MM_Writer;
 
+// Create a tridiagnol matrix
 RCP<crs_mat_t> make_A(RCP<const map_t> map, int n)
 { // Construct empty A, diagnol and off diag components
   RCP <crs_mat_t> A (new crs_mat_t (map,0));
@@ -63,21 +67,21 @@ RCP<crs_mat_t> make_A(RCP<const map_t> map, int n)
   size_t numMyElems = map->getNodeNumElements();
   // Loop through rows of A and assign values
   for(local_ordinal_t lclRow = 0;lclRow < (local_ordinal_t) numMyElems;++lclRow)
-  {// Assign Global Row and column Counter (same since working on diagnol)
+  {// Assign Global Row and Column Counter (same since working on diagnol)
     global_ordinal_t gblRow = map->getGlobalElement(lclRow);
     global_ordinal_t gblCol = gblRow;
     // Assign index and values, edge cases first
     if( gblRow == 0)
     { // A(0,0:1) = [diag, off_diag]
       A->insertGlobalValues( gblRow, 
-          tuple<global_ordinal_t> (gblCol, gblCol+1), 
-          tuple<scalar_t> (off_diag_comp, diag_comp));
+          tuple<global_ordinal_t> (gblCol , gblCol+1), 
+          tuple<scalar_t> ( diag_comp , off_diag_comp));
     }
     else if( gblRow == n-1)
     { // A(n-1, n-2:n-1) = [off_diag, diag]
       A->insertGlobalValues( gblRow, 
           tuple<global_ordinal_t> (gblCol-1, gblCol), 
-          tuple<scalar_t> (diag_comp, off_diag_comp));
+          tuple<scalar_t> (off_diag_comp, diag_comp));
     }
     else
     { // A(i, i-1:i+1) = [off_diag, diag, off_diag]
@@ -87,50 +91,49 @@ RCP<crs_mat_t> make_A(RCP<const map_t> map, int n)
     }
   }
   A->fillComplete();
+  MM_Writer::writeSparseFile("matrix_A", A);
   return A;
 }
 
 void CG_solver(
   RCP<ParameterList> pList,
-  RCP<crs_mat_t> A,
-  RCP<vector_t>  x,
-  RCP<vector_t>  b,
-  RCP<LinearProblem> prob)
+  RCP<LinearProblem> prob,
+  std::ostream& out)
 {
-
-  Ifpack2::Factory factory;
-  auto P = factory.create<row_mat_t>("ILUT", A);
-  P->setParameters(*pList);
-  P->initialize();
-  P->compute();
-
-  prob->setLeftPrec(P);
   prob->setProblem();
-  
   RCP<Solver> solver = rcp(new CGSolver(prob, pList));
-
   solver->solve();
-
+  out << "Tolerance = " << solver->achievedTol() << endl;
   return;
 }
 
-RCP<ParameterList> make_ParamList() /*What goes here?*/
+RCP<ParameterList> make_ParamList() 
 {
   RCP<ParameterList> list = rcp(new ParameterList);
-  int krylov_size = 0;
-  int max_iters = 0;
-  double tol = 0;
-  int out_freq = 0;
-  list->set<int>("krylov size", krylov_size);
-  list->set<int>("maximum iterations", max_iters);
-  list->set<double>("tolerance", tol);
-  list->set<int>("output frequency", out_freq);
+  int krylov_size = 100;
+  int max_iters = 100;
+  double tol = 1e-4;
+  double abs_thres = 1.0;
+  int verbosity = 33;
+  int out_style = 1;
+  int block_size = 1;
+  int out_freq = 1;
+  list->set<int>("Num Blocks", krylov_size);
+  list->set<int>("Block Size", block_size);
+  list->set<int>("Maximum Iterations", max_iters);
+  list->set<int>("Verbosity", verbosity);
+  list->set<int>("Output Style", out_style);
+  list->set<int>("Output Frequency", out_freq);
+  list->set<double>("Convergence Tolerance", tol);
+  list->set<double>("asdfasdfasdf", 021.2);
+  list->set<double>("fact: absolute threshold", abs_thres);
   return list;
 }
 
 void solve_Axb( 
     Teuchos::RCP<const Teuchos::Comm<int> >& comm,
-    std::ostream& out,int n)
+    std::ostream& out,
+    int n)
 {
   Tpetra::global_size_t numGlobalEnts = n;
   global_ordinal_t indexBase = 0;
@@ -142,17 +145,24 @@ void solve_Axb(
   x->putScalar(0.0);
   // Create a vector b, randomize entries
   RCP<vector_t> b = rcp(new vector_t(map));
-  b->randomize(); 
+  b->putScalar(1.0);
+  //b->randomize(); 
 
+  // Print norms 
   out << "||x||_2 = " << x->norm2() << endl;
   out << "||b||_2 = " << b->norm2() << endl;
 
+  // Create matrix A
   RCP<crs_mat_t> A = make_A( map, n);
   out << "Matrix A constructed and filled" << endl;
 
+  // Construct problem and solve
   auto prob = rcp(new LinearProblem(A,x,b));
   RCP<ParameterList> pList = make_ParamList();
-  CG_solver( pList, A, x, b, prob);
+  CG_solver( pList, prob, out);
+
+  MM_Writer::writeDenseFile("vector_b", b);
+  MM_Writer::writeDenseFile("vector_x", x);
 
   return;
 }
@@ -161,7 +171,7 @@ void solve_Axb(
 int main(int argc, char** argv)
 {
   // Size of system to solve (A is nxn)
-  int n = 100;
+  int n = 5;
 
   // Set MPI communicator and printing
   Teuchos::oblackholestream blackHole;
@@ -171,12 +181,12 @@ int main(int argc, char** argv)
   int myRank = comm->getRank();
   std::ostream& out = (myRank==0) ? std::cout : blackHole;
 
+  // Create and solve system
   solve_Axb(comm, out, n);
 
   if (myRank==0)
   {
     cout << "Success!" << endl;
   }
-
   return 0;
 }
