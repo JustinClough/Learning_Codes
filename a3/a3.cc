7c9e5b665b8a220ca19b0c66fc37195053ed19a9
JustinClough <clougj@rpi.edu>
2017-05-25 13:54:33 -0400

    Part 1 of questions 1, assignemnt 3

--- a3/a3.cc
+++ a3/a3.cc
@@ -4,22 +4,60 @@
 
 //STL Headers
 #include <iostream>
+#include <set>
+#include <vector>
 
 using std::cout;
 using std::endl;
 
-void operation_1(pGeom geom, pMesh mesh)
+class collection_t
 {
-  // Determine the number of regions using each mesh vertex accounting for the full mesh (including edges on other parts)
+  public:
+    std::set<pMeshEnt> set;
+    void add_unqs(std::vector<pMeshEnt> ents)
+    {// Add element only if unique
+      for(int i=0; i<int(ents.size());i++)
+      {
+        pMeshEnt e = ents[i];
+        if(set.count(e)==0)
+        {
+          set.insert(e);
+        }
+      }
+      return;
+    }
+    int size()
+    { set.size();}
+};
+
+void operation_1(pGeom geom, pMesh mesh)
+{ // Determine the number of regions using each mesh vertex accounting for the full mesh (including edges on other parts)
   // For each part, list only the vertices owned by that part with local vertex number and the number of mesh 
   //    regions that vertex bounds (on all parts).
+
+  // Create a container for pointer to mesh regions
+  collection_t regions;
+
+  // Loop over mesh vertices, place unique adjacent regions in a set
+  pMeshIter it;
+  pMeshEnt vertex;
+  it = mesh->begin(0);
+  while((vertex = mesh->iterate(it)))
+  {
+    std::vector<pMeshEnt> adj_ents;
+    pumi_ment_getAdj(vertex, 3, adj_ents);
+    regions.add_unqs(adj_ents);
+    adj_ents.clear();
+  }
+  mesh->end(it);
+
+  cout << "Regions " << regions.size() << " on " << PCU_Comm_Self()  << endl;
 
   return;
 } //END operation_1()
 
 void operation_2(pGeom geom, pMesh mesh)
-{
-  // Determine the number of edges using each mesh vertex accounting for the full mesh (included other parts).
+{ // Determine the number of edges using each mesh vertex accounting for the full mesh (included other parts).
   // For each part, list only the vertices owned by that part with local vertex number and the number fo mesh
   //    faces that vertex bounds (on all parts).
 
@@ -27,8 +65,7 @@ void operation_2(pGeom geom, pMesh mesh)
 } //END operation_2()
 
 void operation_3(pGeom geom, pMesh mesh)
-{
-  // Count the number of mesh faces on the partition model face before migration.
+{ // Count the number of mesh faces on the partition model face before migration.
   // List the mesh regions on Part 1 that have at least one mesh face on teh partition model face between 1 and 2. 
   // Migrate mesh regions from Part 1 to 2. 
   // Count the number of mesh faces on the partition model face after migration.
