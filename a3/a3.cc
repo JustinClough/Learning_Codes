94c41c7adf7b1c55abe282b66c73e9e229bd3993
JustinClough <clougj@rpi.edu>
2017-05-27 20:09:43 -0400

    Operation 2, assignment 3

--- a3/a3.cc
+++ a3/a3.cc
@@ -95,6 +95,47 @@ void operation_2(pGeom geom, pMesh mesh)
   // For each part, list only the vertices owned by that part with local vertex number and the number fo mesh
   //    faces that vertex bounds (on all parts).
 
+  // Create a container for pointers to mesh edges
+  collection_t edges;
+  
+  // Loop over mesh vertices, place unique adjacent edges in a the container
+  pMeshIter it;
+  pMeshEnt vertex;
+  it = mesh->begin(0);
+  while((vertex=mesh->iterate(it)))
+  {
+    std::vector<pMeshEnt> adj_ents;
+    pumi_ment_getAdj(vertex, 1, adj_ents);
+    edges.add_unqs(adj_ents);
+    adj_ents.clear();
+  }
+  mesh->end(it);
+
+  // Have each process write its results for part 2 to a file
+  std::ofstream Op2Results;
+  std::string fName_s = "Op2Results_Proc" + make_string(pumi_rank()) + ".txt";
+  char* fName = &fName_s[0u];
+  Op2Results.open( fName);
+  Op2Results << "Process " << pumi_rank() << " found " << edges.size() << " edges.\n";
+
+  Op2Results << "\nVertex number owned by this part (G: Global ID, L: Local ID) and number of bounded faces (F):\n";
+  Op2Results << "G:\tL:\tF:\n";
+  it = mesh->begin(0);
+  while((vertex = mesh->iterate(it)))
+  {
+    if(pumi_ment_isOwned(vertex))
+    {
+      int localID = pumi_ment_getID(vertex);
+      int globalID = pumi_ment_getGlobalID(vertex);
+      Op2Results << globalID << "\t";
+      Op2Results << localID << "\t";
+      int num_adj = pumi_ment_getNumAdj(vertex, 2);
+      Op2Results << num_adj << "\n";
+    }
+  }
+  mesh->end(it);
+  Op2Results.close();
+  
   return;
 } //END operation_2()
 
