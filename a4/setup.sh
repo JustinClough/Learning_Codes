592f65dbf6f90a7bffe4f7a7ac4987f7f27baed9
JustinClough <clougj@rpi.edu>
2017-07-03 12:04:25 -0400

    Begins rewrite

--- a4/setup.sh
+++ a4/setup.sh
@@ -2,7 +2,7 @@
 set -x
 [ ! -e build ] && mkdir build
 cd build
-flags='-g -O2'
+flags='-g '
 cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CXX_FLAGS=$flags -DSCOREC_PREFIX=/usr/local/pumi/core/lib/cmake/SCOREC ../
 cd -
 set +x
