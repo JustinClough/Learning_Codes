c8db09120a75e81a7fd3c4e29f0c044f53385f8d
JustinClough <clougj@rpi.edu>
2018-02-05 00:34:08 -0500

    FEA: Outlines forcing assignment

--- FEA/HW1/src/forcing.cpp
+++ FEA/HW1/src/forcing.cpp
@@ -11,3 +11,23 @@ forcingFactory::forcingFactory( mesh1D* mesh, int caseNumber_)
   numDofs = numNodes - 2;
   F = VectorXd::Zero( numDofs);
 }
+
+forcingFactory::create_forcing()
+{
+  for ( int i = 0; i < numDofs; i++)
+  {
+    assign_force( i);
+  }
+
+  std::cout << "F = " << std::endl;
+  std::cout << F << std::endl;
+
+  return;
+}
+
+VectorXd forcingFatory::getForcing()
+{
+  return F;
+}
+
+
