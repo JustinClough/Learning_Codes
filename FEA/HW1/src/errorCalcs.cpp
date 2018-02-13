d68d749e708b1b9e2751350f804b5d09d58a1bbc
JustinClough <clougj@rpi.edu>
2018-02-13 15:49:20 -0500

    FEA: Adds funcs for error vectors

--- FEA/HW1/src/errorCalcs.cpp
+++ FEA/HW1/src/errorCalcs.cpp
@@ -15,6 +15,9 @@ void errorCalcs( std::vector<VectorXd> solutions,
   EC* ec = new EC( solutions, meshes, caseNumber);
 
   ec->calculate_errors();
+  ec->calculate_norms();
+  ec->calculate_orders();
+  ec->write_errors();
 
   delete ec;
   return;
@@ -34,13 +37,170 @@ void EC::calculate_errors()
   int M = 3;
   for( int i = 0; i < ms.size(); i++)
   {
-    int numDofs   = ms[i]->getNumNodes() - 1;
-    VectorXd e    = VectorXd::Zero( M * numDofs)
-    VectorXd ePos = VectorXd::Zero( M * numDofs)
+    mesh1D*  mesh    = ms[i];
+    VectorXd sol     = sols[i];
 
+    int      numDofs = mesh->getNumNodes() - 1;
+    VectorXd e       = VectorXd::Zero( M * numDofs);
+    VectorXd ePos    = VectorXd::Zero( M * numDofs);
+
+    // Calculate the position error vector
+    error_positions( ePos, mesh, M);
+
+    // Calc the error vector at each position (including interpolation)
+    eval_errors( e, ePos, sol, mesh, M);
+
+    std::cout << "es[" << i << "] = " << std::endl;
+    std::cout << e << std::endl;
 
     es.push_back( e);
     xE.push_back( ePos);
   }
+  return;
+}
+
+void EC::eval_errors( VectorXd& e, 
+                      VectorXd& ePos, 
+                      VectorXd& sol, 
+                      mesh1D*   mesh,
+                      int       M)
+{
+  int numElems = mesh->getNumElems();
+  int numNodes = mesh->getNumNodes();
+  for( int i = 0; i < numElems; i++)
+  {
+    double uim1 = 0.0;
+    double ui = 0.0;
+    if( i == 0)
+    {
+      // Assuming the boundary is enforced
+      uim1 = get_analytic( 0.0);
+    }
+    else
+    {
+      uim1 = sol( i - 1);
+    }
+
+    if( i == (numElems - 1))
+    {
+      // Assuming the boundary is enforced
+      ui = get_analytic( 1.0);
+    }
+    else
+    {
+      ui = sol( i);
+    }
+
+    elem element = mesh->getElem( i);
+    double error = 0.0;
+    for( int j = 0; j < M; j++)
+    {
+      int    m     = j + 1;
+      int    index = (i * M) + j;
+
+      double xj = ePos( index);
+      double U  = get_analytic( xj);
+      double UI = get_interp( uim1, ui, element, xj);
+
+      error     = UI - U;
+      e( index) = error;
+    }
+  }
+
+  return;
+}
+
+double EC::get_interp( double ua, double ub, elem element, double x)
+{
+  double h   = element.getLength();
+  double xa  = element.getLeftPos();
+  double xb  = element.getRightPos();
+
+  if ( (x < xa) || (xb < x) )
+  {
+    std::cout << "Interpolating outside of element!" << std::endl
+              << "xa = " << xa << std::endl
+              << "xb = " << xb << std::endl
+              << "x = "  << x  << std::endl;
+  }
+
+  double uIa = ua * (xb - x) / h;
+  double uIb = ub * (x - xa) / h;
+
+  double uI  = uIa + uIb;
+  return uI;
+}
+
+double EC::get_analytic( double x)
+{
+  double U = 0.0;
+  if( cn == 1 || cn == 2)
+  {
+    U = x * (x - 1.0) * (std::sin( 5.0 * x) + 3.0 * std::exp( x));
+  }
+  else if ( cn == 3)
+  {
+    U = 4.0;
+  }
+  else if ( cn == 4)
+  {
+    U = x - 2.0;
+  }
+  else if ( cn == 5)
+  {
+    U = (x * x) - 3.0;
+  }
+  else
+  {
+    std::cout << "Bad case number of " << cn 
+              << ". Aborting..." << std::endl;
+    std::abort();
+  }
+
+  return U;
+}
+
+void EC::error_positions( VectorXd& ePos, mesh1D* mesh, int M)
+{
+  int numElems = mesh->getNumElems();
+  for( int i = 0; i < numElems; i++)
+  {
+    elem   element = mesh->getElem( i);
+    double xim1    = element.getLeftPos();
+    double hi      = element.getLength();
+
+    double m  = 0.0;
+    double MM = 3.0;
+    for( int j = 0; j < M; j++)
+    {
+      m = m + 1.0;
+      double x     = xim1 + hi * ( m / MM);
+      int    index = (i * M) + j;
+      ePos( index) = x;
+    }
+  }
+  return;
+}
+
+void EC::calculate_norms()
+{
+  for( int i = 0; i < es.size(); i++)
+  {
+    VectorXd e  = es[i];
+    //double   L2 = e.cwise
+
+  }
+  return;
+}
+
+void EC::calculate_orders()
+{
+  //TODO
+  return;
+}
+
+void EC::write_errors()
+{
+  //TODO
   return;
 }
--- FEA/HW1/src/errorCalcs.hpp
+++ FEA/HW1/src/errorCalcs.hpp
@@ -19,6 +19,15 @@ class EC
     // Calculates the error vectors for all meshes of this case
     void calculate_errors();
 
+    // Writes the errors to a text file
+    void write_errors();
+
+    // Calculates the norms of the error vectors
+    void calculate_norms();
+
+    // Calculate the order of convergence
+    void calculate_orders();
+
   private:
     // The vector of FEM solutions
     std::vector<VectorXd> sols;
@@ -34,6 +43,36 @@ class EC
 
     // The vector of error positions
     std::vector<VectorXd> xE;
+
+    // Populate the error position vector
+    void error_positions( VectorXd& ePos, mesh1D* mesh, int M);
+
+    // Populate the error value vector
+    void eval_errors( VectorXd& e, 
+                      VectorXd& ePos, 
+                      VectorXd& sol, 
+                      mesh1D*   mesh,
+                      int       M);
+
+    // Get the analytical solution at a give position
+    double get_analytic( double x);
+
+    // Get the interpolation of the FEM solution
+    double get_interp( double ua, 
+                       double ub,  
+                       elem element, 
+                       double x);
+
+    // The vector of L_2 norms
+    std::vector<double> L_2;
+
+    // The vector of L_inf norms
+    std::vector<double> L_inf;
+
+    // The vector of NRG norms
+    std::vector<double> NRG;
+
+    
 };
 
 #endif
