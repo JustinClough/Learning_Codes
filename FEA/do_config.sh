22766614f3847f9b2e6cf7d1d63828bd9c8f333a
JustinClough <clougj@rpi.edu>
2018-03-20 12:38:15 -0400

    FEA: Begins HW5; adds -pedantic to CMAKE

--- FEA/do_config.sh
+++ FEA/do_config.sh
@@ -1,7 +1,10 @@
 export EIG_SRC=/lore/clougj/Learning_Codes/FEA/Eigen_Package
 export FEA_HW1_SRC=/lore/clougj/Learning_Codes/FEA/HW1/src
+export FEA_HW5_SRC=/lore/clougj/Learning_Codes/FEA/HW5/src
 
 cmake \
   -D EIG_DIR=${EIG_SRC} \
+  -D CMAKE_CXX_FLAGS="-g -O2 -Wall" \
   -D HW1_SRC=${FEA_HW1_SRC} \
+  -D HW5_SRC=${FEA_HW5_SRC} \
   ../
