09b15edc24ee7befcf7a1d36bf36c81eae5a703b
JustinClough <clougj@rpi.edu>
2018-03-31 21:15:33 -0400

    FEA: main takes in arg's for cn and np1

--- FEA/HW5/src/fea_hw5.cpp
+++ FEA/HW5/src/fea_hw5.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <cstdlib>
 #include "driver.hpp"
 
 int main( int argc, char** argv)
 {
+  if ( argc != 3)
+  {
+    std::cout 
+      << "Usage: hw5 Case_Number N+1 "
+      << std::endl;
+
+    std::abort();
+  }
+
+  int cn  = std::atoi( argv[1]);
+  int np1 = std::atoi( argv[2]);
+
+  std::cout << "cn = " << cn << std::endl;
+  std::cout << "np1 = " << np1 << std::endl;
+
+  if( cn <= 0 || cn > 4)
+  {
+    std::cout 
+      << "Unrecognized Case Number = "
+      << cn 
+      << std::endl;
+
+    std::abort();
+  }
+
   seed_random();
 
-  int CaseNumber = 0;
-  //int Np1[2] = { 20, 40};
 
-  int Np1[2] = { 3, 3};
-
-
-  for( int i = 0; i < 2; i++)
-  {
-    drive_problem( CaseNumber, Np1[i]);  
-  }
+  drive_problem( cn, np1);  
 
   return 0;
 }
