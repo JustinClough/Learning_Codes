0077c89d01b9b293ed8eb458b28f13ff2e4a1917
JustinClough <clougj@rpi.edu>
2018-04-12 08:28:57 -0400

    FEA: Begins hw6; adds to config and CMake

--- FEA/do_config.sh
+++ FEA/do_config.sh
@@ -1,10 +1,12 @@
 export EIG_SRC=/lore/clougj/Learning_Codes/FEA/Eigen_Package
 export FEA_HW1_SRC=/lore/clougj/Learning_Codes/FEA/HW1/src
 export FEA_HW5_SRC=/lore/clougj/Learning_Codes/FEA/HW5/src
+export FEA_HW6_SRC=/lore/clougj/Learning_Codes/FEA/HW6/src
 
 cmake \
   -D EIG_DIR=${EIG_SRC} \
   -D CMAKE_CXX_FLAGS="-g -O3 -Wall -Werror -lrt" \
   -D HW1_SRC=${FEA_HW1_SRC} \
   -D HW5_SRC=${FEA_HW5_SRC} \
+  -D HW6_SRC=${FEA_HW6_SRC} \
   ../
