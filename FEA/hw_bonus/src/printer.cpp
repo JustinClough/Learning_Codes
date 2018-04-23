c95f8a32c5a86b5cbd1166606834f1a495e3d9a6
JustinClough <clougj@rpi.edu>
2018-04-23 18:04:37 -0400

    FEA: Adds printer class

--- FEA/hw_bonus/src/printer.cpp
+++ FEA/hw_bonus/src/printer.cpp
@@ -1,3 +1,76 @@
 #include "printer.hpp"
 
+#include <iostream>
+#include <cmath>
 
+Printer::Printer()
+{
+  entries = 0;
+}
+
+Printer::~Printer()
+{
+  mesh_sizes.clear();
+  L2.clear();
+  Linf.clear();
+  H1.clear();
+
+  L2_order.clear();
+  Linf_order.clear();
+  H1_order.clear();
+}
+
+void Printer::add( int N, double L2_, double Linf_, double H1_)
+{
+  mesh_sizes.push_back( N);
+  L2.push_back( L2_);
+  Linf.push_back( Linf_);
+  H1.push_back( H1_);
+
+  entries++;
+
+  return;
+}
+
+void Printer::calc_order(
+                std::vector< double>& errors,
+                std::vector< double>& orders)
+{
+  double divlog2 = 1.0 / std::log( 2.0);
+
+  double now = 0.0;
+  double old = 0.0;
+  double ans = 0.0;
+  for( size_t i = 0; i < errors.size(); i++)
+  {
+    if( i != 0)
+    {
+      old = errors[i-1];
+      now = errors[i];
+
+      ans = std::log( old / now) * divlog2;
+    }
+
+    orders.push_back( ans);
+  }
+
+  return;
+}
+
+void Printer::calc_orders()
+{
+  calc_order( L2, L2_order);
+  calc_order( Linf, Linf_order);
+  calc_order( H1, H1_order);
+
+  return;
+}
+
+void Printer::print()
+{
+  calc_orders();
+
+  std::cout << std::scientific;
+  // TODO
+  return;
+}
--- FEA/hw_bonus/src/printer.hpp
+++ FEA/hw_bonus/src/printer.hpp
@@ -1,4 +1,55 @@
 #ifndef PRINTER_HPP
 #define PRINTER_HPP
 
+#include <vector>
+
+class Printer
+{
+  public:
+    // Constructor
+    Printer();
+
+    // Destructor
+    ~Printer();
+  
+    // Print collected data to screen
+    void print();
+
+    // Adds solution data
+    void add( int N, double L2_, double Linf_, double H1_);
+
+  private:
+    // Amount of data collected
+    int entries; 
+
+    // Vector of mesh sizes
+    std::vector< int> mesh_sizes;
+
+    // Vector of L2 errors
+    std::vector< double> L2;
+
+    // Vector of Linf errors
+    std::vector< double> Linf;
+
+    // Vector of H1 errors
+    std::vector< double> H1;
+
+    // Vector of L2 orders
+    std::vector< double> L2_order;
+
+    // Vector of Linf orders
+    std::vector< double> Linf_order;
+
+    // Vector of H1 orders
+    std::vector< double> H1_order;
+
+    // Calculates the orders of convergence
+    void calc_orders();
+
+    // Calcuates the order for one vector
+    void calc_order(
+          std::vector< double>& errors,
+          std::vector< double>& orders);
+};
+
 #endif 
