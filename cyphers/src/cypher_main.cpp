683089dd71f83dfc005eb399ee3e0606bb005a0d
JustinClough <justin.clough1@gmail.com>
2018-11-08 07:08:12 -0800

    cypher project main

---
+++ cyphers/src/cypher_main.cpp
@@ -0,0 +1,21 @@
+#include "input_handler.hpp"
+#include "cypher_class.hpp"
+
+int main( int argc, char** argv)
+{
+  Cypher* cyph = new Cypher();
+  if( !parse_inputs( argc, argv, cyph))
+  {
+    std::cout 
+    << "Usage:" << std::endl
+    << argv[0] << " base target.txt output.txt"
+    << std::endl;
+  
+    std::abort();
+  }
+
+  delete cyph;
+
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
