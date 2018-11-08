697cecf0b3d8293bd5bc12e8bd50a0f24f122035
JustinClough <justin.clough1@gmail.com>
2018-11-08 06:40:59 -0800

    Starts simple cypher project

---
+++ cyphers/do_config.sh
@@ -0,0 +1,8 @@
+export CYPHER_HOME=/home/jlclough/Learning_Codes/cyphers
+export CYPHER_SRC=${CYPHER_HOME}/src
+export CYPHER_BLD=${CYPHER_HOME}/build
+
+cmake \
+  -D CYPHER_SRC=${CYPHER_SRC} \
+  -D CMAKE_CXX_FLAGS="-g -O3 -Wall -Werror -lrt" \
+  ../
